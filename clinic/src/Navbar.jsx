7c1c51a1425f4f384bf2db773bec0eae87d92283
AhmedK22 <ahmedkhaledabdallah22@gmail.com>
2023-11-04 19:20:39 +0200

    second change

--- clinic/src/Navbar.jsx
+++ clinic/src/Navbar.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-export default function Navbar(props) {
+export default function Navbar() {
   return (
     <>
      <nav className="navbar navbar-expand-lg navbar-light ">
         <div className="container-fluid">
             <h2>ONNMED</h2>
-            {props.userData? <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
                     <li className="nav-item active me-2"><button type="submit" className="btn btn-primary"
                             >Register</button></li>
                     <li className="nav-item active "><button type="submit" className="btn btn-primary">Login</button></li>
                 </ul>
-            </div>:""}
+            </div>
            
         </div>
     </nav>
